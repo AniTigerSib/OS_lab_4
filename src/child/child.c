70f091e74feb2c8137958bc0db32a78788d81221
AniTigerSib <khudoleev04@mail.ru>
2024-04-04 17:28:59 +0500

    develop/Changed some logic, added defines as constants

--- src/child/child.c
+++ src/child/child.c
@@ -2,18 +2,20 @@
 
 int main() {
     int* sequence = NULL;
-    int buff_int, size, flag = 0;
+    int buff_int, size = 0;
+    int flag = FLAG_IS_NORMAL;
     char buff_char = '\0';
-    if (scanf("%d%c", &size, &buff_char) != 2 || buff_char != '\n') {
+
+    if (scanf("%d", &size) != 1) {
         printf("Error: expected size of sequence in following format: {integer num}{\\n}\n");
     } else {
         for (int i = 0; i < size && !flag; i++) {
             if (scanf("%d%c", &buff_int, &buff_char) != 2) {
                 printf("Error: expected data in following format: {integer num}{space}\n");
-                flag = 1;
+                flag = FLAG_IS_ERROR;
             } else if (buff_char != ' ' && buff_char != '\n') {
                 printf("Error: expected %d or %d, got %d\n", ' ', '\n', buff_char);
-                flag = 1;
+                flag = FLAG_IS_ERROR;
             } else {
                 sequence = (int*) realloc(sequence, (i + 1) * sizeof(int));
                 sequence[i] = buff_int;
@@ -22,7 +24,10 @@ int main() {
         if (sequence == NULL) {
             printf("Error: empty sequence or some problems here");
         } else {
-            if (flag == 0) {
+            printf("Sequence: ");
+            PrintSequence(sequence, size);
+            printf("\n\nSorted sequence: ");
+            if (flag == FLAG_IS_NORMAL) {
                 Qsort(sequence, size);
                 PrintSequence(sequence, size);
             }
@@ -30,5 +35,4 @@ int main() {
         }
     }
     exit(flag);
-    // return flag;
 }
--- src/child/child.h
+++ src/child/child.h
@@ -6,5 +6,7 @@
 
 #include "../sequence/sequence_functions.h"
 
+#define FLAG_IS_ERROR 1
+#define FLAG_IS_NORMAL 0
 
 #endif
